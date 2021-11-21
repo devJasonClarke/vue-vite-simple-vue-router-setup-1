dd89a919b0d62c57ce83cc671c40dc56b8e6e991
devJasonClarke <jettison200@gmail.com>
2021-11-21 15:32:34 -0500

    ⚡ Got everything up and working ⚡

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <router-link to="/">Home</router-link>
+  </div>
+  <h2>{{ home }}</h2>
+</template>
+
+<script >
+// Options API
+export default {
+  data: () => ({home: "Home Page, here I come!"}),
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <router-link to="/about">About</router-link>
+  </div>
+  <h2>{{ about }}</h2>
+</template>
+
+<script setup>
+// Composition API
+import { ref } from "vue";
+
+const about = ref("Go to the About Page");
+</script>
+
+<style lang="scss" scoped></style>
